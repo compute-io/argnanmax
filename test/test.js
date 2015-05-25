d5e6bb57df9475496e49beb10b3215a663341460
Planeshifter <pburckhardt@outlook.com>
2015-05-25 07:26:50 -0400

    [UPDATE] tests

--- test/test.js
+++ test/test.js
@@ -45,6 +45,29 @@ describe( 'compute-argnanmax', function tests() {
 		}
 	});
 
+
+	it( 'should throw an error if provided an accessor argument which is not a function', function test() {
+		var values = [
+			'5',
+			5,
+			true,
+			undefined,
+			null,
+			NaN,
+			[],
+			{}
+		];
+
+		for ( var i = 0; i < values.length; i++ ) {
+			expect( badValue( values[i] ) ).to.throw( TypeError );
+		}
+		function badValue( value ) {
+			return function() {
+				argmax( [1,2,3], value );
+			};
+		}
+	});
+
 	it( 'should compute the maximum value ignoring non-numeric values and return the corresponding indices', function test() {
 		var data, expected, actual;
 
@@ -55,12 +78,33 @@ describe( 'compute-argnanmax', function tests() {
 		assert.deepEqual( actual, expected );
 	});
 
-	it( 'should return an empty array if an array does not contain numeric values', function test() {
-		var data;
 
-		data = [ 'beep', 'boop', 'bap', null ];
+	it( 'should compute the maximum value and return the corresponding indices using an accessor', function test() {
+		var data, expected, actual;
 
-		assert.deepEqual( argmax(data), [] );
+		data = [
+			{'x':-8},
+			{'x':null},
+			{'x':-20},
+			{'x':-50},
+			{'x':null},
+			{'x':-30},
+			{'x':-8},
+			{'x':-20},
+		];
+
+		actual = argmax( data, getValue );
+		expected = [ 0, 6 ];
+
+		assert.deepEqual( actual, expected );
+
+		function getValue( d ) {
+			return d.x;
+		}
+	});
+
+	it( 'should return null if provided an empty array', function test() {
+		assert.isNull( argmax( [] ) );
 	});
 
 });
